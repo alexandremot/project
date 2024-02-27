8468b2e1b1453f1ce9e77b7cf78cf1e5389dc341
Alexandre Mota <alexandremot@gmail.com>
2024-02-26 23:59:46 -0300

    hello world (string)

---
+++ src/main/java/com/example/project/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package com.example.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class GreetingController {
+    
+    @GetMapping("/hello")
+    public String greeting() {
+        return "Hello world";
+    }
+}
