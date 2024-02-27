4389267bc3cca7172f4999ab10eb9b0c5f095fce
Alexandre Mota <alexandremot@gmail.com>
2024-02-27 00:06:48 -0300

    incluida model Greetings

--- src/main/java/com/example/project/controller/GreetingController.java
+++ src/main/java/com/example/project/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.project.controller;
 
+import com.example.project.model.Greetings;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class GreetingController {
+
+    Greetings greetings = new Greetings("Hello world");
     
     @GetMapping("/hello")
-    public String greeting() {
-        return "Hello world";
+    public Greetings greeting() {
+        return greetings;
     }
 }
